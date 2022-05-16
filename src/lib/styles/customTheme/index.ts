87cb6fb5a804b532b9d97663d52d55a37aab9ca8
ilhamadhim <ilhamm179@gmail.com>
2022-05-16 19:12:17 +0700

    chore: change default color mode -> dark

--- src/lib/styles/customTheme/index.ts
+++ src/lib/styles/customTheme/index.ts
@@ -1,15 +1,17 @@
 import { extendTheme } from "@chakra-ui/react";
 
-import colors from "./colors";
 import Button from "./components/button";
 import fonts from "./fonts";
 
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
 const customTheme = extendTheme({
+  config,
   fonts,
-  colors,
-  components: {
-    Button,
-  },
+  components: { Button },
 });
 
 export default customTheme;
