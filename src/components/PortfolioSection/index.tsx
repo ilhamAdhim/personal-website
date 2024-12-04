16eca3a61a56f32d00f35df75d644d89b2616b36
ilhamadhim <ilhamm179@gmail.com>
2024-12-04 16:53:23 +0700

    WIP: Update wording in about section

--- src/components/PortfolioSection/index.tsx
+++ src/components/PortfolioSection/index.tsx
@@ -2,6 +2,7 @@ import {
   Avatar,
   Box,
   Divider,
+  Flex,
   Heading,
   Text,
   VStack,
@@ -18,8 +19,8 @@ const PortfolioAbout = () => {
 
   return (
     <VStack
-      p={10}
-      top={[0, 0, 0, 10, 10, 40]}
+      p={8}
+      top={[0, 0, 0, 10, 10, 24]}
       spacing={[4, 4, 4, 4, 3, 5]}
       rounded="lg"
       position="sticky"
@@ -53,18 +54,37 @@ const PortfolioAbout = () => {
       </Text>
 
       <Divider />
-      <Box>
-        <Text
-          align="center"
-          data-aos={isSmallViewport ? "fade-down" : ""}
-          data-aos-delay={isSmallViewport ? "1300" : 0}
-        >
-          {" "}
-          Current Favorite Tech Stack ⚡
-        </Text>
-        <TechStackList />
-      </Box>
+      <Flex w="full" gap={4} flexDir={["column", "row"]}>
+        <Box w={["full", "50%"]}>
+          <Text
+            fontSize={14}
+            align="center"
+            data-aos={isSmallViewport ? "fade-down" : ""}
+            data-aos-delay={isSmallViewport ? "1300" : 0}
+          >
+            {" "}
+            Favorite Tech Stack ⚡
+          </Text>
+          <TechStackList category="fav" />
+        </Box>
 
+        <Box>
+          <Divider orientation="vertical" />
+        </Box>
+
+        <Box w={["full", "50%"]}>
+          <Text
+            fontSize={14}
+            align="center"
+            data-aos={isSmallViewport ? "fade-down" : ""}
+            data-aos-delay={isSmallViewport ? "1300" : 0}
+          >
+            {" "}
+            Other Tech Stack 🎯
+          </Text>
+          <TechStackList category="other" />
+        </Box>
+      </Flex>
       <Divider />
       <Box>
         <Text fontSize="1.2em" align="center" mb="4">
--- src/data/techStackList.tsx
+++ src/data/techStackList.tsx
@@ -4,25 +4,62 @@ import {
   SiNextdotjs,
   SiReact,
   SiSass,
+  SiSupabase,
+  SiTailwindcss,
   SiTypescript,
   SiWebpack,
 } from "react-icons/si";
 
 import type { ITechStackProps } from "types/TechStackListProps";
 
-const techStack: ITechStackProps[] = [
+export const techStack: ITechStackProps[] = [
   {
     id: 1,
-    icon: <SiNextdotjs />,
-    color: "teal",
-    link: "https://nextjs.org/docs/getting-started",
-    description: "After getting React basics, learning",
-    descWithLink: "Next JS",
+    icon: <SiChakraui />,
+    color: "cyan",
+    link: "https://chakra-ui.com/",
+    description: "Chakra UI has been my go-to UI library for a while now.",
+    descWithLink: "It's well-documented",
     nextDesc:
-      "was seamless to me. SEO optimized, dynamic paths, and high customability are features I love at this moment.",
+      "and i can build an app faster without any hassle. Plus, there are lots of open-source components are available out there.",
   },
+
   {
     id: 2,
+    icon: <SiJavascript />,
+    color: "#F0DB4F",
+    link: "https://github.com/ilhamAdhim/restopedia",
+    description:
+      "JS Basics are essential for learning and implementing React. To enforce that idea, I developed",
+    descWithLink: "PWA + SPA web app with vanilla JS.",
+    nextDesc:
+      "With these hassle, i can appreciate more of library and frameworks existence.",
+  },
+  {
+    id: 3,
+    icon: <SiWebpack />,
+    color: "#1C78C0",
+    link: "https://webpack.js.org/",
+    description: "",
+    descWithLink: "Webpack",
+    nextDesc:
+      "is a module bundler that i used for Module Analyzer and code splitting. Plus, we can differentiate production and development mode.",
+  },
+  {
+    id: 4,
+    icon: <SiSass />,
+    color: "pink",
+    link: "https://sass-lang.com/documentation",
+    description: "While CSS is easy to use.",
+    descWithLink: "Sass",
+    nextDesc:
+      "can do a lot better compared to normal CSS. Some features i've been using are global variables, mixins, and functions",
+  },
+];
+
+const favTechStack: ITechStackProps[] = [
+  {
+    id: 1,
     icon: <SiReact />,
     color: "cyan",
     link: "https://reactjs.org/docs/hooks-intro.html",
@@ -30,6 +67,16 @@ const techStack: ITechStackProps[] = [
       "I started learning React in 2019. It enabled me to create a SPA (Single Page Application) with high interactivity. When",
     descWithLink: "React Hooks",
     nextDesc: "comes out, i like it due to its simplicity.",
+  },
+  {
+    id: 2,
+    icon: <SiNextdotjs />,
+    color: "teal",
+    link: "https://nextjs.org/docs/getting-started",
+    description: "After getting React basics, learning",
+    descWithLink: "Next JS",
+    nextDesc:
+      "was seamless to me. SEO optimized, dynamic paths, and high customability are features I love at this moment.",
   },
   {
     id: 3,
@@ -43,46 +90,25 @@ const techStack: ITechStackProps[] = [
   },
   {
     id: 4,
-    icon: <SiChakraui />,
-    color: "cyan",
-    link: "https://chakra-ui.com/",
-    description: "Chakra UI has been my go-to UI library for a while now.",
-    descWithLink: "It's well-documented",
+    icon: <SiSupabase />,
+    color: "teal.300",
+    link: "https://supabase.com/",
+    description:
+      "A Backend as a Service tools that i use for fullstack application.",
+    descWithLink: "Supabase",
     nextDesc:
-      "and i can build an app faster without any hassle. Plus, there are lots of open-source components are available out there.",
+      "helps with easy OAuth, logs, and built-in support of making CRUD operations, calling RPCs and setting up CRON too. ",
   },
-
   {
     id: 5,
-    icon: <SiJavascript />,
-    color: "#F0DB4F",
-    link: "https://github.com/ilhamAdhim/restopedia",
-    description:
-      "JS Basics are essential for learning and implementing React. To enforce that idea, I developed",
-    descWithLink: "PWA + SPA web app with vanilla JS.",
+    icon: <SiTailwindcss />,
+    color: "#007acc",
+    link: "https://tailwindcss.com/docs/installation",
+    description: "For design that needs to be high customized and flexibility",
+    descWithLink: "Tailwind CSS",
     nextDesc:
-      "With these hassle, i can appreciate more of library and frameworks existence.",
-  },
-  {
-    id: 6,
-    icon: <SiWebpack />,
-    color: "#1C78C0",
-    link: "https://webpack.js.org/",
-    description: "",
-    descWithLink: "Webpack",
-    nextDesc:
-      "is a module bundler that i used for Module Analyzer and code splitting. Plus, we can differentiate production and development mode.",
-  },
-  {
-    id: 7,
-    icon: <SiSass />,
-    color: "pink",
-    link: "https://sass-lang.com/documentation",
-    description: "While CSS is easy to use.",
-    descWithLink: "Sass",
-    nextDesc:
-      "can do a lot better compared to normal CSS. Some features i've been using are global variables, mixins, and functions",
+      "is a no-brainer option. Combined it with ShadCN, to makes it even more robust.",
   },
 ];
 
-export default techStack;
+export default favTechStack;
