593ff3278e431e2daa525e5c4693d77154f56c3b
ilhamadhim <ilhamm179@gmail.com>
2025-02-13 21:31:06 +0700

    logs: added error console for api/articles

--- src/pages/api/articles.ts
+++ src/pages/api/articles.ts
@@ -38,6 +38,7 @@ export default async function handler(
 
     return res.status(200).json(list);
   } catch (error) {
+    console.error("error", error);
     return res.status(500).json({ error: "Failed to read data" });
   }
 }
